80c9141c1317d6dcbd86ed42d5d3f5a597f4846c
eddiebee <edbeeotudor@gmail.com>
2022-05-11 21:12:18 +0100

    add colors from styled-guide specs

--- src/themes.ts
+++ src/themes.ts
@@ -1,10 +1,24 @@
 import { createTheme } from "@mui/material";
 import { green, purple } from "@mui/material/colors";
 
+const primaryColors = {
+    moderateBlue: "hsl(238, 40%, 52%)",
+    softRed: "hsl(358, 79%, 66%)",
+    lightGrayishBlue: "hsl(239, 57%, 85%)",
+    paleRed: "hsl(357, 100%, 86%)",
+  },
+  neutralColors = {
+    darkBlue: "hsl(212, 24%, 26%)",
+    grayishBlue: "hsl(211, 10%, 45%)",
+    lightGray: "hsl(223, 19%, 93%)",
+    veryLightGray: "hsl(228, 33%, 97%)",
+    white: "hsl(0, 0%, 100%)",
+  };
+
 export const theme = createTheme({
   palette: {
     primary: {
-      main: purple[500],
+      main: primaryColors["moderateBlue"],
     },
     secondary: {
       main: green[500],
