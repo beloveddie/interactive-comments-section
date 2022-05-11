c2300150c9fa4583527ac4c12ba1bdf339eaa042
eddiebee <edbeeotudor@gmail.com>
2022-05-11 20:51:37 +0100

    Setup app theme using MUI theme system

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { ThemeProvider } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import { theme } from "./themes";
+
 function App() {
   return (
-    <h1>
-      Interactive comments section.... <i>coming soon</i>
-    </h1>
+    <ThemeProvider theme={theme}>
+      <h1>
+        Interactive comments section.... <i>coming soon</i>
+      </h1>
+      <Button variant="contained">All good</Button>
+    </ThemeProvider>
   );
 }
 
